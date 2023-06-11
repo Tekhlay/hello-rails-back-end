ecd8b55a6c9036b08d74d547318d6426ef7515b2
Teklay <teklay@gmail.com>
2023-06-11 12:48:39 +0300

    Update cors and messages

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MessagesController < ApplicationController
   def index
     random_message = Message.order('RANDOM()').first
-    render json: { message: random_message.text }
+    render json: { greeting: random_message.text }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,24 @@
 Rails.application.routes.draw do
+
   root 'page#index'
+
   namespace :api do
+
     namespace :v1 do
-      get 'messages/index'
+
+      resources :messages, only: [:index]
+
     end
+
   end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+
+
   # Defines the root path route ("/")
+
   # root "articles#index"
+
 end
+
